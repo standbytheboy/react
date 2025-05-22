00944523e5898b796e9cdb33da29e03f90632e2e
standbytheboy <moraislcs05@gmail.com>
2025-05-22 01:21:57 -0300

    adicionando funcionalidade de excluir

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Tasks from "./components/Tasks";
+import "./App.css";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar Programação",
+      description:
+        "Estudar programação para se tornar um desenvolvedor full stack",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Programação 2",
+      description:
+        "Estudar programação para se tornar um desenvolvedor full stack",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar Programação 3",
+      description:
+        "Estudar programação para se tornar um desenvolvedor full stack",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id == taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function deleteTask(taskId) {
+    const newTasks = tasks.filter((task => task.id != taskId)); //ele mantém todos os itens da lista, menos o que eu deletar
+    setTasks(newTasks);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-10">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} deleteTask={deleteTask}></Tasks> {/* coloco todas as funções que preciso usar no arquivo "Tasks" */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,27 @@
+import { ChevronRight, DeleteIcon, Trash2Icon, TrashIcon } from "lucide-react";
+
+
+{/* pra não ter que fazer isso: "function Tasks(props) {}" e ficar chamando props.nomeDaFuncao, podemos fazer da seguinte maneira: */} 
+function Tasks({tasks, onTaskClick, deleteTask}) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow-2xs">
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={`w-full text-left bg-slate-400 text-white p-2 rounded-md cursor-pointer ${task.isCompleted && "line-through"}`}>
+            {task.title}
+          </button>
+          <button className="bg-slate-400 text-white p-2 rounded-md cursor-pointer">
+            <ChevronRight />
+          </button>
+          <button onClick={() => deleteTask(task.id)} className="bg-slate-400 text-white p-2 rounded-md cursor-pointer">
+            <TrashIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tasks;
